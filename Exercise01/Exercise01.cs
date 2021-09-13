3d6ed1861215b3789a146266538786fbac41bfec
cingalia <seth@inficore.co.ke>
2021-09-13 21:31:06 +0300

    Updated exersise 01 and 02

--- Exercise01/Exercise01.cs
+++ Exercise01/Exercise01.cs
@@ -4,53 +4,135 @@ namespace Exercise01
 {
     public static class Exercise01
     {
-        public static string Towards(int number)
+        public static string Towards(System.Numerics.BigInteger n)
         {
-            if (number == 0)
+            if (n < 0)
+                throw new NotSupportedException("negative numbers not supported");
+            if (n == 0)
                 return "zero";
-
-            if (number < 0)
-                return "minus " + NumberToWords(Math.Abs(number));
-
-            string words = "";
-
-            if ((number / 1000000) > 0)
-            {
-                words += NumberToWords(number / 1000000) + " million ";
-                number %= 1000000;
-            }
-
-            if ((number / 1000) > 0)
-            {
-                words += NumberToWords(number / 1000) + " thousand ";
-                number %= 1000;
-            }
-
-            if ((number / 100) > 0)
-            {
-                words += NumberToWords(number / 100) + " hundred ";
-                number %= 100;
-            }
-
-            if (number > 0)
-            {
-                if (words != "")
-                    words += "and ";
-
-                var unitsMap = new[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
-                var tensMap = new[] { "zero", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
-
-                if (number < 20)
-                    words += unitsMap[number];
-                else
-                {
-                    words += tensMap[number / 10];
-                    if ((number % 10) > 0)
-                        words += "-" + unitsMap[number % 10];
-                }
-            }
-
-            return words;
+            if (n < 10)
+                return ConvertDigitToString(n);
+            if (n < 20)
+                return ConvertTeensToString(n);
+            if (n < 100)
+                return ConvertHighTensToString(n);
+            if (n < 1000)
+                return ConvertBigNumberToString(n, (int)1e2, "hundred");
+            if (n < (System.Numerics.BigInteger) 1e6)
+                return ConvertBigNumberToString(n, (int)1e3, "thousand");
+            if (n < (System.Numerics.BigInteger) 1e9)
+                return ConvertBigNumberToString(n, (int)1e6, "million");
+            if (n < (System.Numerics.BigInteger) 1e12)
+                return ConvertBigNumberToString(n, (int)1e9, "billion");
+            if (n < (System.Numerics.BigInteger) 1e15)
+                return ConvertBigNumberBigToString(n, (System.Numerics.BigInteger)1e12, "trillion");
+            if (n < (System.Numerics.BigInteger) 1e18)
+                return ConvertBigNumberBigToString(n, (System.Numerics.BigInteger)1e15, "quadrillion");
+            if (n < (System.Numerics.BigInteger) 1e21)
+                return ConvertBigNumberBigToString(n, (System.Numerics.BigInteger)1e18, "quintillion");
+            //if (n < 1e12)
+            return ConvertBigNumberToString(n, (int)1e9, "Nonillion");
         }
+
+        private static string ConvertDigitToString(System.Numerics.BigInteger _i)
+        {
+            long i = long.Parse(_i.ToString());
+            switch (i)
+            {
+                case 0: return "";
+                case 1: return "one";
+                case 2: return "two";
+                case 3: return "three";
+                case 4: return "four";
+                case 5: return "five";
+                case 6: return "six";
+                case 7: return "seven";
+                case 8: return "eight";
+                case 9: return "nine";
+                default:
+                    throw new IndexOutOfRangeException(String.Format("{0} not a digit", i));
+            }
+        }
+
+        //assumes a number between 10 & 19
+        private static string ConvertTeensToString(System.Numerics.BigInteger _n)
+        {
+            long n = long.Parse(_n.ToString());
+            switch (n)
+            {
+                case 10: return "ten";
+                case 11: return "eleven";
+                case 12: return "twelve";
+                case 13: return "thirteen";
+                case 14: return "fourteen";
+                case 15: return "fiveteen";
+                case 16: return "sixteen";
+                case 17: return "seventeen";
+                case 18: return "eighteen";
+                case 19: return "nineteen";
+                default:
+                    throw new IndexOutOfRangeException(String.Format("{0} not a teen", n));
+            }
+        }
+
+        //assumes a number between 20 and 99
+        private static string ConvertHighTensToString(System.Numerics.BigInteger _n)
+        {
+            long n = long.Parse(_n.ToString());
+            int tensDigit = (int)(Math.Floor((double)n / 10.0));
+
+            string tensStr;
+            switch (tensDigit)
+            {
+                case 2: tensStr = "twenty"; break;
+                case 3: tensStr = "thirty"; break;
+                case 4: tensStr = "forty"; break;
+                case 5: tensStr = "fifty"; break;
+                case 6: tensStr = "sixty"; break;
+                case 7: tensStr = "seventy"; break;
+                case 8: tensStr = "eighty"; break;
+                case 9: tensStr = "ninety"; break;
+                default:
+                    throw new IndexOutOfRangeException(String.Format("{0} not in range 20-99", n));
+            }
+            if (n % 10 == 0) return tensStr;
+            string onesStr = ConvertDigitToString(n - tensDigit * 10);
+            return tensStr + "-" + onesStr;
+        }
+
+        private static string ConvertBigNumberToString(System.Numerics.BigInteger n, int baseNum, string baseNumStr)
+        {
+            // special case: use commas to separate portions of the number, unless we are in the hundreds
+            string separator = (baseNumStr != "hundred") ? ", " : " ";
+
+            // Strategy: translate the first portion of the number, then recursively translate the remaining sections.
+            // Step 1: strip off first portion, and convert it to string:
+            System.Numerics.BigInteger bigPart = (System.Numerics.BigInteger)(Math.Floor((double)n / baseNum));
+            string bigPartStr = Towards(bigPart) + " " + baseNumStr;
+            // Step 2: check to see whether we're done:
+            if (n % baseNum == 0) return bigPartStr;
+            // Step 3: concatenate 1st part of string with recursively generated remainder:
+            System.Numerics.BigInteger restOfNumber = n - bigPart * baseNum;
+            return bigPartStr + separator + Towards(restOfNumber);
+        }
+
+        private static string ConvertBigNumberBigToString(System.Numerics.BigInteger n, System.Numerics.BigInteger baseNum, string baseNumStr)
+        {
+            // special case: use commas to separate portions of the number, unless we are in the hundreds
+            string separator = (baseNumStr != "hundred") ? ", " : " ";
+
+            // Strategy: translate the first portion of the number, then recursively translate the remaining sections.
+            // Step 1: strip off first portion, and convert it to string:
+            //double result = Math.Exp(BigInteger.Log(x) - BigInteger.Log(y));
+            System.Numerics.BigInteger bigPart = (System.Numerics.BigInteger)Math.Exp(System.Numerics.BigInteger.Log(n) - System.Numerics.BigInteger.Log(baseNum));
+            //System.Numerics.BigInteger bigPart = (System.Numerics.BigInteger)(Math.Floor(n / baseNum));
+            string bigPartStr = Towards(bigPart) + " " + baseNumStr;
+            // Step 2: check to see whether we're done:
+            if (n % baseNum == 0) return bigPartStr;
+            // Step 3: concatenate 1st part of string with recursively generated remainder:
+            System.Numerics.BigInteger restOfNumber = n - bigPart * baseNum;
+            return bigPartStr + separator + Towards(restOfNumber);
+        }
+
     }
 }
--- Exercise02/Program.cs
+++ Exercise02/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace Exercise02
 {
@@ -6,7 +7,36 @@ namespace Exercise02
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Get user input
+            Console.WriteLine("Please enter numeric value");
+            var _input = Console.ReadLine();
+
+            var input = Regex.Replace(_input, ",", "");
+
+            System.Numerics.BigInteger amount_int = System.Numerics.BigInteger.Parse(input);
+
+            var _output = Exercise01.Exercise01.Towards(amount_int);
+
+            var output = ReplaceLast(",", " and ", _output);
+
+            Console.WriteLine(output);
+
+            Console.ReadKey();
+        }
+
+        static string ReplaceLast(string find, string replace, string str)
+        {
+            int lastIndex = str.LastIndexOf(find);
+
+            if (lastIndex == -1)
+            {
+                return str;
+            }
+
+            string beginString = str.Substring(0, lastIndex);
+            string endString = str.Substring(lastIndex + find.Length);
+
+            return beginString + replace + endString;
         }
     }
 }
