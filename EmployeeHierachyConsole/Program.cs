94fbb27a79d04f8d8ae39347c415e6d2408b3944
cingalia <seth@inficore.co.ke>
2021-09-14 11:39:37 +0300

    updated Salary budget

--- EmployeeHierachyConsole/Program.cs
+++ EmployeeHierachyConsole/Program.cs
@@ -8,6 +8,7 @@ namespace EmployeeHierachyConsole
 {
     class Program
     {
+        private static List<Hierarchy> hierarchyList = new List<Hierarchy>();
         static void Main(string[] args)
         {
             Console.WriteLine("Testing");
@@ -15,14 +16,14 @@ namespace EmployeeHierachyConsole
             StringBuilder csvData = new StringBuilder();
             csvData.AppendLine("Emplyee4,Employee2,500");
             csvData.AppendLine("Employee3,Employee1,800");
-            //csvData.AppendLine("Employee6,,800");
+            csvData.AppendLine("Employee6,Employee2,500");
             csvData.AppendLine("Employee1,,1000");
             csvData.AppendLine("Employee5,Employee1,500");
             csvData.AppendLine("Employee2,Employee1,500");
 
             var _csvData = csvData.ToString();
 
-            var hierarchyList = new List<Hierarchy>();
+            
 
             //Validate Salary
             foreach (var lineData in _csvData.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries))
@@ -41,13 +42,13 @@ namespace EmployeeHierachyConsole
 
             //Validate employee
 
-            if ((from e in hierarchyList
-                 group e by e.EmployeeId into g
-                 where g.Count() > 1
-                 select g.Key).Count() > 0)
-            {
-                throw new InvalidOperationException("Employee contains more than one manager");
-            }
+            //if ((from e in hierarchyList
+            //     group e by e.EmployeeId into g
+            //     where g.Count() > 1
+            //     select g.Key).Count() > 0)
+            //{
+            //    throw new InvalidOperationException("Employee contains more than one manager");
+            //}
 
             //Validate CEO
             if (hierarchyList.Where(x => string.IsNullOrEmpty(x.ManagerId)).ToList().Count > 1)
@@ -55,7 +56,100 @@ namespace EmployeeHierachyConsole
                 throw new InvalidOperationException("More than one CEO");
             }
 
+            //ValidateCircularReference
+            var successors = hierarchyList.ToDictionary(x => x.EmployeeId, x => x.ManagerId);
+
+            var visited = new HashSet<string>();
+
+            List<List<string>> cycles = new List<List<string>>();
+
+            foreach (var hierarchy in hierarchyList)
+            {
+                string cycleStart = FindCycle(hierarchy.EmployeeId, successors, visited);
+
+                if (cycleStart != null)
+                {
+                    // cycle found, get detail information about involved nodes
+                    //List<string> cycle = GetCycleMembers(cycleStart, successors);
+                    //cycles.Add(cycle);
+                }
+            }
+
+            //Validate Manager
+            foreach (var hierarchy in hierarchyList)
+            {
+                if (!string.IsNullOrEmpty(hierarchy.ManagerId))
+                {
+                    var data = hierarchyList.Where(x => x.EmployeeId == hierarchy.ManagerId);
+                    if (data.Count() <= 0)
+                    {
+                        throw new InvalidOperationException("Manager is not an EMployee");
+                    }
+                }
+                
+            }
+
+            //Salary Budget
+            long allSalary = 0;
+            var managerDetails = hierarchyList.FirstOrDefault(x => x.EmployeeId == "Employee3");
+            allSalary = managerDetails.Salary;
+            if (managerDetails != null)
+            {
+                var employees = hierarchyList.Where(x => x.ManagerId == managerDetails.EmployeeId).ToList();
+                foreach (var employee in employees)
+                {
+                    allSalary = allSalary + EmployeesSalary(employee.EmployeeId);
+                }
+            }
+
+
             Console.ReadKey();
+        }
+        static long EmployeesSalary(string managerId)
+        {
+            long _salary = 0;
+            var managerDetails = hierarchyList.FirstOrDefault(x => x.EmployeeId == managerId);
+            _salary = managerDetails.Salary;
+            if (managerDetails != null)
+            {
+                _salary = _salary + hierarchyList.Where(x => x.ManagerId == managerDetails.EmployeeId).Sum(x => x.Salary);
+            }
+            return _salary;
+        }
+
+        static string FindCycleHelper(string start, Dictionary<string, string> successors, HashSet<string> stackVisited, HashSet<string> previouslyVisited)
+        {
+            string current = start;
+            while (current != null)
+            {
+                if (previouslyVisited.Contains(current))
+                {
+                    return null;
+                }
+                if (stackVisited.Contains(current))
+                {
+                    // this node is part of a cycle
+                    return current;
+                }
+
+                stackVisited.Add(current);
+
+                successors.TryGetValue(current, out current);
+            }
+
+            return null;
+        }
+
+        static string FindCycle(string start, Dictionary<string, string> successors, HashSet<string> globalVisited)
+        {
+            HashSet<string> stackVisited = new HashSet<string>();
+
+            var result = FindCycleHelper(start, successors, stackVisited, globalVisited);
+
+            // update collection of previously processed nodes
+            globalVisited.UnionWith(stackVisited);
+
+            return result;
         }
     }
 }
--- EmployeeHierarchy/Employees.cs
+++ EmployeeHierarchy/Employees.cs
@@ -22,6 +22,40 @@ namespace EmployeeHierarchy
 
             //Validate CEO
             ValidateCEO();
+
+            //Circular Reference Validation
+            ValidateCR();
+
+            //Validate Manager
+            ValidateManager();
+        }
+
+        public long SalaryBudget(string managerId)
+        {
+            long allSalary = 0;
+            var managerDetails = hierarchyList.FirstOrDefault(x => x.EmployeeId == managerId);
+            allSalary = managerDetails.Salary;
+            if (managerDetails != null)
+            {
+                var employees = hierarchyList.Where(x => x.ManagerId == managerDetails.EmployeeId).ToList();
+                foreach (var employee in employees)
+                {
+                    allSalary = allSalary + EmployeesSalary(employee.EmployeeId);
+                }
+            }
+            return allSalary;
+        }
+
+        long EmployeesSalary(string managerId)
+        {
+            long _salary = 0;
+            var managerDetails = hierarchyList.FirstOrDefault(x => x.EmployeeId == managerId);
+            _salary = managerDetails.Salary;
+            if (managerDetails != null)
+            {
+                _salary = _salary + hierarchyList.Where(x => x.ManagerId == managerDetails.EmployeeId).Sum(x => x.Salary);
+            }
+            return _salary;
         }
 
         //Validate Salaries
@@ -61,6 +95,74 @@ namespace EmployeeHierarchy
             {
                 throw new InvalidOperationException("More than one CEO");
             }
+        }
+
+        //Circular Reference Validation
+        private void ValidateCR()
+        {
+            var successors = hierarchyList.ToDictionary(x => x.EmployeeId, x => x.ManagerId);
+
+            var visited = new HashSet<string>();
+
+            List<List<string>> cycles = new List<List<string>>();
+
+            foreach (var hierarchy in hierarchyList)
+            {
+                string cycleStart = FindCycle(hierarchy.EmployeeId, successors, visited);
+
+                if (cycleStart != null)
+                {
+                    // cycle found
+                    throw new InvalidOperationException("Circular Reference Found");
+                }
+            }
+        }
+
+        private void ValidateManager()
+        {
+            foreach (var hierarchy in hierarchyList)
+            {
+                if (!string.IsNullOrEmpty(hierarchy.ManagerId))
+                {
+                    var data = hierarchyList.Where(x => x.EmployeeId == hierarchy.ManagerId);
+                    if (data.Count() <= 0)
+                    {
+                        throw new InvalidOperationException("Manager is not an EMployee");
+                    }
+                }
+
+            }
+        }
+
+        static string FindCycleHelper(string start, Dictionary<string, string> successors, HashSet<string> stackVisited, HashSet<string> previouslyVisited)
+        {
+            string current = start;
+            while (current != null)
+            {
+                if (previouslyVisited.Contains(current))
+                {
+                    return null;
+                }
+                if (stackVisited.Contains(current))
+                {
+                    // this node is part of a cycle
+                    return current;
+                }
+
+                stackVisited.Add(current);
+
+                successors.TryGetValue(current, out current);
+            }
+
+            return null;
+        }
+        static string FindCycle(string start, Dictionary<string, string> successors, HashSet<string> globalVisited)
+        {
+            HashSet<string> stackVisited = new HashSet<string>();
+            var result = FindCycleHelper(start, successors, stackVisited, globalVisited);
+            // update collection of previously processed nodes
+            globalVisited.UnionWith(stackVisited);
+            return result;
         }
     }
 }
